ce9044df1623751aecc1deb2218757b539281171
Rubén Fernández Farelo <90851489+ru-farelo@users.noreply.github.com>
2025-06-12 13:10:35 +0200

    Update testConnect.js

--- backend/testConnect.js
+++ backend/testConnect.js
@@ -3,11 +3,11 @@ import { sql, config } from './config/db.js';
 async function testConnection() {
   try {
     const pool = await sql.connect(config);
-    console.log('✅ Conexión exitosa a la base de datos');
+    console.log(' Conexión exitosa a la base de datos');
     await pool.close();
     process.exit(0);
   } catch (error) {
-    console.error('❌ Error de conexión:', error);
+    console.error(' Error de conexión:', error);
     process.exit(1);
   }
 }
